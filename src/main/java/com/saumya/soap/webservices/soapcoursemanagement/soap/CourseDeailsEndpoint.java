1eed4988d42f3bb862e0b3d1a23c5bb7945e6359
saumyashah7 <saumyashahori18@gmail.com>
2020-08-20 22:14:54 -0700

    Added endpoint for XML Request and Response

---
+++ src/main/java/com/saumya/soap/webservices/soapcoursemanagement/soap/CourseDeailsEndpoint.java
@@ -0,0 +1,27 @@
+package com.saumya.soap.webservices.soapcoursemanagement.soap;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.saumya.courses.CourseDetails;
+import com.saumya.courses.GetCourseDetailsRequest;
+import com.saumya.courses.GetCourseDetailsResponse;
+
+@Endpoint
+public class CourseDeailsEndpoint {
+	
+	@PayloadRoot(namespace="http://saumya.com/courses", localPart="GetCourseDetailsRequest")
+	@ResponsePayload
+	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) 
+	{
+		GetCourseDetailsResponse response=new GetCourseDetailsResponse();
+		CourseDetails csd=new CourseDetails();
+		csd.setId(request.getId());
+		csd.setName("Microservices Course");
+		csd.setDescription("Wonderful learning!");
+		return response;
+	}
+
+}
